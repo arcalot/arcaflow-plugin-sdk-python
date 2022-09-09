bca4289b1aaa8323ae22086153544b64a73ea0be
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2022-09-09 21:21:41 +0200

    Fixes #25: API / transport protocol

---
+++ src/arcaflow_plugin_sdk/atp.py
@@ -0,0 +1,159 @@
+"""
+The Arcaflow Transport Protocol is a protocol to communicate between the Engine and a plugin. This library provides
+the services to do so. The main use-case is running over STDIO, so the communication protocol is designed to run 1:1,
+it is not possible to use multiple clients (engines) with a plugin.
+
+The message flow is as follows:
+
+|   sequenceDiagram
+|     participant E as Engine
+|       participant P as Plugin
+|       autonumber
+|       P ->> E: Schema information
+|       E ->> P: Start step
+|       P ->> E: Execution results
+"""
+import dataclasses
+import io
+import signal
+import sys
+import typing
+import cbor2
+from arcaflow_plugin_sdk import schema
+
+
+@dataclasses.dataclass
+class HelloMessage:
+    """
+    This message is the the initial greeting message a plugin sends to the output.
+    """
+    version: typing.Annotated[
+        int,
+        schema.min(1),
+        schema.name("Version"),
+        schema.description("Version number for the protocol"),
+    ]
+    schema: typing.Annotated[
+        schema.Schema,
+        schema.name("Schema"),
+        schema.description(
+            "Schema information describing the required inputs and outputs, as well as the steps offered by this "
+            "plugin."
+        )
+    ]
+
+
+_HELLO_MESSAGE_SCHEMA = schema.build_object_schema(HelloMessage)
+
+
+def _handle_exit(_signo, _stack_frame):
+    print("Exiting normally")
+    sys.exit(0)
+
+
+def run_plugin(
+        s: schema.SchemaType,
+        stdin: io.FileIO,
+        stdout: io.FileIO,
+        stderr: io.FileIO,
+) -> int:
+    """
+    This function wraps running a plugin.
+    """
+    signal.signal(signal.SIGTERM, _handle_exit)
+    try:
+        decoder = cbor2.decoder.CBORDecoder(stdin)
+        encoder = cbor2.encoder.CBOREncoder(stdout)
+
+        start = HelloMessage(1, s)
+        serialized_message = _HELLO_MESSAGE_SCHEMA.serialize(start)
+        encoder.encode(serialized_message)
+
+        message = decoder.decode()
+    except SystemExit:
+        return 0
+    try:
+        if message["id"] is None:
+            stderr.write("Work start message is missing the 'id' field.")
+            return 1
+        if message["config"] is None:
+            stderr.write("Work start message is missing the 'config' field.")
+            return 1
+        original_stdout = sys.stdout
+        original_stderr = sys.stderr
+        out_buffer = io.StringIO()
+        sys.stdout = out_buffer
+        sys.stderr = out_buffer
+        output_id, output_data = s.call_step(message["id"], s.unserialize_input(message["id"], message["config"]))
+        sys.stdout = original_stdout
+        sys.stderr = original_stderr
+        encoder.encode({
+            "output_id": output_id,
+            "output_data": s.serialize_output(message["id"], output_id, output_data),
+            "debug_logs": out_buffer.getvalue(),
+        })
+    except SystemExit:
+        return 1
+    return 0
+
+
+class PluginClientStateException(Exception):
+    """
+    This
+    """
+    msg: str
+
+    def __init__(self, msg: str):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+
+class PluginClient:
+    """
+    This is a rudimentary client that reads information from a plugin and starts work on the plugin. The functions
+    must be executed in order.
+    """
+    stdin: io.FileIO
+    stdout: io.FileIO
+    decoder: cbor2.decoder.CBORDecoder
+
+    def __init__(
+        self,
+        stdin: io.FileIO,
+        stdout: io.FileIO,
+    ):
+        self.stdin = stdin
+        self.stdout = stdout
+        self.decoder = cbor2.decoder.CBORDecoder(stdout)
+        self.encoder = cbor2.encoder.CBOREncoder(stdin)
+
+    def read_hello(self) -> HelloMessage:
+        """
+        This function reads the intial "Hello" message from the plugin.
+        """
+        message = self.decoder.decode()
+        return _HELLO_MESSAGE_SCHEMA.unserialize(message)
+
+    def start_work(self, step_id: str, input_data: any):
+        """
+        After the Hello message has been read, this function starts work in a plugin with the specified data.
+        """
+        self.encoder.encode({
+            "id": step_id,
+            "config": input_data,
+        })
+
+    def read_results(self) -> (str, any, str):
+        """
+        This function reads the results of an execution from the plugin.
+        """
+        message = self.decoder.decode()
+        if message["output_id"] is None:
+            raise PluginClientStateException("Missing 'output_id' in CBOR message. Possibly wrong order of calls?")
+        if message["output_data"] is None:
+            raise PluginClientStateException("Missing 'output_data' in CBOR message. Possibly wrong order of calls?")
+        if message["debug_logs"] is None:
+            raise PluginClientStateException("Missing 'output_data' in CBOR message. Possibly wrong order of calls?")
+        return message["output_id"], message["output_data"], message["debug_logs"]
---
+++ src/arcaflow_plugin_sdk/test_atp.py
@@ -0,0 +1,104 @@
+import dataclasses
+import os
+import signal
+import time
+import unittest
+from typing import Tuple, Union, TextIO
+
+from arcaflow_plugin_sdk import plugin, atp, schema
+
+
+@dataclasses.dataclass
+class Input:
+    name: str
+
+
+@dataclasses.dataclass
+class Output:
+    message: str
+
+
+@plugin.step(
+    id="hello-world",
+    name="Hello world!",
+    description="Says hello :)",
+    outputs={"success": Output},
+)
+def hello_world(params: Input) -> Tuple[str, Union[Output]]:
+    print("Hello world!")
+    return "success", Output(
+        "Hello, {}!".format(params.name))
+
+
+test_schema = plugin.build_schema(hello_world)
+
+
+class ATPTest(unittest.TestCase):
+
+    def _execute_plugin(self) -> Tuple[int, TextIO, TextIO]:
+        stdin_reader_fd, stdin_writer_fd = os.pipe()
+        stdout_reader_fd, stdout_writer_fd = os.pipe()
+        pid = os.fork()
+        if pid == 0:
+            os.close(stdin_writer_fd)
+            os.close(stdout_reader_fd)
+
+            stdin_reader = os.fdopen(stdin_reader_fd, "r")
+            stdout_writer = os.fdopen(stdout_writer_fd, "w")
+
+            result = atp.run_plugin(
+                test_schema,
+                stdin_reader.buffer.raw,
+                stdout_writer.buffer.raw,
+                stdout_writer.buffer.raw,
+            )
+            os.close(stdin_reader_fd)
+            os.close(stdout_writer_fd)
+            if result != 0:
+                print("Plugin exited with non-zero status: {}".format(result))
+                os._exit(1)
+            os._exit(0)
+        elif pid > 0:
+            os.close(stdin_reader_fd)
+            os.close(stdout_writer_fd)
+
+            stdin_writer = os.fdopen(stdin_writer_fd, "w")
+            stdout_reader = os.fdopen(stdout_reader_fd, "r")
+
+            return pid, stdin_writer, stdout_reader
+        else:
+            self.fail("Fork failed")
+
+    def _cleanup(self, pid, stdin_writer, stdout_reader):
+        stdin_writer.close()
+        stdout_reader.close()
+        time.sleep(1)
+        os.kill(pid, signal.SIGTERM)
+        stop_info = os.waitpid(pid, 0)
+        exit_status = os.waitstatus_to_exitcode(stop_info[1])
+        if exit_status != 0:
+            self.fail("Plugin exited with non-zero status: {}".format(exit_status))
+
+    def test_full_workflow(self):
+        pid, stdin_writer, stdout_reader = self._execute_plugin()
+
+        try:
+            client = atp.PluginClient(stdin_writer.buffer.raw, stdout_reader.buffer.raw)
+            hello_message = client.read_hello()
+            self.assertEqual(1, hello_message.version)
+
+            self.assertEqual(
+                schema.SCHEMA_SCHEMA.serialize(test_schema),
+                schema.SCHEMA_SCHEMA.serialize(hello_message.schema)
+            )
+
+            client.start_work("hello-world", {"name": "Arca Lot"})
+
+            output_id, output_data, debug_logs = client.read_results()
+            self.assertEqual("Hello world!\n", debug_logs)
+        finally:
+            self._cleanup(pid, stdin_writer, stdout_reader)
+
+
+if __name__ == '__main__':
+    unittest.main()
