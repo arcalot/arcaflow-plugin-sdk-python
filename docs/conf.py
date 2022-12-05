4fc93334b373db9ad79930f4ec226d537211a620
Peter Portante <peter.portante@redhat.com>
2022-12-05 11:17:01 -0500

    Fix wayward python path insertion for docs

--- docs/conf.py
+++ docs/conf.py
@@ -3,12 +3,6 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-import sys
-
-sys.path.insert(0, "/home/sbonazzo/NotBackedUp/upstream/arcaflow-plugin-sdk-python/src")
-
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
