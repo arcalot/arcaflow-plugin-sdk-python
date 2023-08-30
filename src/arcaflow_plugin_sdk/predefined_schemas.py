9113031c5b81ad3228e0b9d834e502bcb9386656
jaredoconnell <joconnel@redhat.com>
2023-08-30 15:23:49 -0400

    Fix wrong signal ID
    Also refactored to improve the code

--- src/arcaflow_plugin_sdk/predefined_schemas.py
+++ src/arcaflow_plugin_sdk/predefined_schemas.py
@@ -2,19 +2,22 @@ from dataclasses import dataclass
 
 from arcaflow_plugin_sdk import schema
 
+
+cancel_signal_input_schema = schema.ScopeSchema(
+    {
+        "cancelInput": schema.ObjectSchema(
+            "cancelInput",
+            {},  # No fields/properties at the moment
+        )
+    },
+    "cancelInput",
+)
+
 cancel_signal_schema = schema.SignalSchema(
-    id="cancelInput",
-    data_schema=schema.ScopeSchema(
-        {
-            "cancelInput": schema.ObjectSchema(
-                "cancelInput",
-                {},  # No fields/properties at the moment
-            )
-        },
-        "cancelInput",
-    ),
+    id="cancel",
+    data_schema=cancel_signal_input_schema,
     display=schema.DisplayValue(
-        name="Cancel input",
+        name="Step Cancel",
         description="The signal that instructs the plugin to finish execution gracefully.",
         icon=None,
     ),
