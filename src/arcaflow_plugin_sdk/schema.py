9a7d2446b0921aab2a44b67ccb36bbf8670d104c
Matthew F Leader <mleader@redhat.com>
2024-04-29 16:37:08 -0400

    fix

--- src/arcaflow_plugin_sdk/schema.py
+++ src/arcaflow_plugin_sdk/schema.py
@@ -2643,9 +2643,7 @@ class OneOfSchema(_JSONSchemaGenerator, _OpenAPIGenerator):
     ) -> any:
         one_of = []
         for k, v in self.types.items():
-            # noinspection PyProtectedMember
             scope.objects[v.id]._to_jsonschema_fragment(scope, defs)
-
             self._insert_discriminator(defs.defs[v.id], str(k))
             if v.display is not None:
                 if v.display.name is not None:
@@ -2663,18 +2661,15 @@ class OneOfSchema(_JSONSchemaGenerator, _OpenAPIGenerator):
         one_of = []
         discriminator_mapping = {}
         for k, v in self.types.items():
-            # noinspection PyProtectedMember
             scope.objects[v.id]._to_openapi_fragment(scope, defs)
             name = v.id + self.oneof_type + str(k)
             discriminator_mapping[k] = "#/components/schemas/" + name
-
             self._insert_discriminator(defs.defs[v.id], str(k))
             if v.display is not None:
                 if v.display.name is not None:
                     defs.defs[v.id]["title"] = v.display.name
                 if v.display.description is not None:
                     defs.defs[v.id]["description"] = v.display.description
-
             defs.components[name] = defs.defs[v.id]
             one_of.append({"$ref": "#/components/schemas/" + name})
         return {
